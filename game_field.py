2aed1aaa1e9a7dea610222b90e09f1430f07d75b
Ech012 <itamaralony9@gmail.com>
2026-08-12 14:33:11 +0300

    SDf

--- game_field.py
+++ game_field.py
@@ -1,5 +1,6 @@
 import random
 
+
 import consts
 
 def create():
@@ -35,6 +36,18 @@ def put_bushes(matricx_game):
                 count += 1
     return matricx_game
 
+def put_soldier(matricx_game):
+    for row in range(0, 6):
+        for col in range(0, 2):
+            matricx_game[row][col] = consts.SOLDIER
+
+    return matricx_game
+
 def return_matricx():
     matrix = create()
+    matrix = put_bombs(matrix)
+    matrix_bushes = put_bushes(matrix)
+    matrix = put_soldier(matrix)
+    return matrix, matrix_bushes
+print(return_matricx())
 
