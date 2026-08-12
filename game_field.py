4b9eaf5df6c695b00231873db005801526950ec2
liron-sy <qlyrwn2@gmail.com>
2026-08-12 10:19:38 +0300

    my first commit

---
+++ game_field.py
@@ -0,0 +1,17 @@
+import consts
+
+matricx_game = []
+x =
+
+for row in range(25):
+    for col in range(50):
+        matricx_game.append(consts.EMPTY_BLOCK)
+
+for row in range (21,23):
+    for col in range (46,49):
+        matricx_game[row][col] = consts.FLAG
+
+for row in range(25):
+    for col in range(50):
+        matricx_game[row][col] = randon
+
